fc2a330181e95553048720972188c49ccd94193e
Dhiraj Kumar <thedhirajofficials@gmail.com>
2025-08-02 20:10:35 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
 
+const mongoose=require('mongoose');
+const userschema=mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    contact:String,
+    gender:String,
+    address:String
+   
+})
+module.exports=mongoose.model('user',userschema);
+
